d17cfaf8b9772a371f22c3fb7b88e82b0b3a280f
shumonbalok <shumonbalok@gmail.com>
2019-08-20 07:51:50 -0700

    integrate paystack

--- resources/views/frontend/checkouts/paystack.blade.php
+++ resources/views/frontend/checkouts/paystack.blade.php
@@ -1,10 +1,11 @@
-
+<form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal" role="form">
         <!-- <div class="row" style="margin-bottom:40px;">
           <div class="col-md-8 col-md-offset-2"> -->
             <input type="hidden" name="email" value="shumonbalok@gmail.com"> {{-- required --}}
-{{-- For other necessary things you want to add to your payload. it is optional though --}}
-            <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
-            <input type="hidden" name="key" value="{{ config('paystack.secretKey') }}"> {{-- required --}}
+            <input type="hidden" name="orderID" value="{{Auth::user()->id .'-'. date('dmy')}}">
+            <input type="hidden" name="amount" value="{{str_replace(',', '', $carts->total()) + session('shipping_method')}}"> {{-- required in kobo --}}
+            <input type="hidden" name="quantity" value="3">
+            
             {{ csrf_field() }} {{-- works only when using laravel 5.1, 5.2 --}}
             <input data-toggle="tooltip" 
                   @if(session('payment_method') && session('shipping_method') && session('shipping_address'))
@@ -15,3 +16,4 @@
                   class="btn" id="button-confirm" value="Confirm Order">
           <!-- </div>
         </div> -->
+</form>
