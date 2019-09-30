8f13e1d30529a817f810f0747c05f228b81c442a
shumon pal <shumonbalok@gmail.com>
2019-09-30 07:20:08 -0700

    Fix the stripe integretion

--- config/services.php
+++ config/services.php
@@ -31,8 +31,8 @@ return [
 
     'stripe' => [
         'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+        'key' => 'pk_test_TaKlzQBjUwEgvpSoZxWXKP9F00OPEv8udI',
+        'secret' => 'sk_test_eMHwh8GwaNAdhCyY5vVN30PC00Jq8ecRVh',
         'version' => '2016-07-06',
     ],
 
