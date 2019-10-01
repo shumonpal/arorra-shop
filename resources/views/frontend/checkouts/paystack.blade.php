f3b766363485a82682a6cc6f281c21666f5053e5
shumon pal <shumonbalok@gmail.com>
2019-10-01 05:12:48 -0700

    Paystack test

--- resources/views/frontend/checkouts/paystack.blade.php
+++ resources/views/frontend/checkouts/paystack.blade.php
@@ -3,11 +3,13 @@
           <div class="col-md-8 col-md-offset-2"> -->
             <input type="hidden" name="email" value="shumonbalok@gmail.com"> {{-- required --}}
             <input type="hidden" name="orderID" value="{{Auth::user()->id .'-'. date('dmy')}}">
-            <input type="hidden" name="amount" value="{{str_replace(',', '', $carts->total()) + session('shipping_method')}}"> {{-- required in kobo --}}
+            <!-- <input type="hidden" name="amount" value="{{str_replace(',', '', $carts->total()) + session('shipping_method')}}"> {{-- required in kobo --}} -->
+            <input type="hidden" name="amount" value="45363"> {{-- required in kobo --}}
             <input type="hidden" name="quantity" value="3">
             <input type="hidden" name="metadata" value="{{ json_encode($array = ['user_id' => Auth::user()->id]) }}" > {{-- For other necessary things you want to add to your payload. it is optional though --}}
-            <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
-            <input type="hidden" name="key" value="{{ config('paystack.secretKey') }}"> {{-- required --}}
+            <input type="hidden" name="reference" value="#475497htbr"> {{-- required --}}
+            <!-- <input type="hidden" name="key" value="{{ config('paystack.secretKey') }}"> {{-- required --}} -->
+            <input type="hidden" name="key" value="pk_test_a664c7c289b5577970604c5f988265d4778f75ed"> {{-- required --}}
             {{ csrf_field() }} {{-- works only when using laravel 5.1, 5.2 --}}
             <input data-toggle="tooltip" 
                   @if(session('payment_method') && session('shipping_method') && session('shipping_address'))
