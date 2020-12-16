07bea4a010d90dff8474496476b9a35964515e93
Shumon Chandrapal <shumonpal1@gmail.com>
2020-12-16 16:12:48 +0800

    Add Cors Middleware

--- routes/api.php
+++ routes/api.php
@@ -27,25 +27,32 @@ Route::group([
 
 });
 
-//products
-Route::get('products', 'ProductController@index');
-Route::get('product/{id}', 'ProductController@show');
-Route::get('product-new', 'ProductController@newArrived');
-Route::get('product-feature', 'ProductController@featured');
-Route::get('product-week-deals', 'ProductController@weekDeals');
-Route::get('product-special-deals', 'ProductController@specialDeals');
-Route::get('product-related/{id}', 'ProductController@related');
-Route::match(['get', 'post'], '/filter', 'ProductController@filter');
+Route::group([
 
-//brands
-Route::get('brands', 'BrandController@index');
-Route::get('products-by-brand/{id}', 'BrandController@productsByBrand');
+    'middleware' => 'cors',
 
-//category
-Route::get('categories', 'CategoryController@index');
-Route::get('products-by-category/{id}', 'CategoryController@productsBycategory');
+], function () {
 
-//subcategory
-Route::get('subcategories', 'SubCategoryController@index');
-Route::get('products-by-subcategory/{id}', 'SubCategoryController@productsBysubcategory');
+    //products
+    Route::get('products', 'ProductController@index');
+    Route::get('product/{id}', 'ProductController@show');
+    Route::get('product-new', 'ProductController@newArrived');
+    Route::get('product-feature', 'ProductController@featured');
+    Route::get('product-week-deals', 'ProductController@weekDeals');
+    Route::get('product-special-deals', 'ProductController@specialDeals');
+    Route::get('product-related/{id}', 'ProductController@related');
+    Route::match(['get', 'post'], '/filter', 'ProductController@filter');
 
+    //brands
+    Route::get('brands', 'BrandController@index');
+    Route::get('products-by-brand/{id}', 'BrandController@productsByBrand');
+
+    //category
+    Route::get('categories', 'CategoryController@index');
+    Route::get('products-by-category/{id}', 'CategoryController@productsBycategory');
+
+    //subcategory
+    Route::get('subcategories', 'SubCategoryController@index');
+    Route::get('products-by-subcategory/{id}', 'SubCategoryController@productsBysubcategory');
+
+});
