6f6fc73c37fe852cd11885d1dc774ce33562d1f0
Shumon Chandrapal <shumonpal1@gmail.com>
2020-12-23 18:47:10 +0800

    Add cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,9 +39,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'cors',
             'throttle:60,1',
-            'bindings',
-            'cors'
+            'bindings'
+
         ],
     ];
 
