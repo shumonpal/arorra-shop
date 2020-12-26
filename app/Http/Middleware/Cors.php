72573d8828b1bb10f87f2fce1d418325eb34e7e6
Shumon Chandrapal <shumonpal1@gmail.com>
2020-12-26 16:53:47 +0800

    Add cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', "*")
             ->header('Access-Control-Allow-Methods', "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-            ->header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Accept, Content-Type, Authorization")
-            ->header('Access-Control-Request-Headers', "Origin, X-Requested-With, Accept, Content-Type, Authorization");
+            ->header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Accept, Content-Type, authorization")
+            ->header('Access-Control-Request-Headers', "Origin, X-Requested-With, Accept, Content-Type, authorization");
     }
 }
